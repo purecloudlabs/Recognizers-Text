cf1ebd39a9276e7930e8a29759e9319d273ea6f1
Sanjeev Halyal <sanjeev.halyal@genesys.com>
2021-12-20 15:56:24 +0000

    fix build.sh

--- Python/build.sh
+++ Python/build.sh
@@ -1,42 +1,42 @@
 cd libraries/resource-generator
 
 echo // Installing Resource Generator Dependencies
-$1/bin/pip install -r ./requirements.txt
+pip install -r ./requirements.txt
 
 echo // Building Resources
-$1/bin/python index.py ../recognizers-choice/resource-definitions.json
-$1/bin/python index.py ../recognizers-number/resource-definitions.json
-$1/bin/python index.py ../recognizers-number-with-unit/resource-definitions.json
-$1/bin/python index.py ../recognizers-date-time/resource-definitions.json
-$1/bin/python index.py ../recognizers-sequence/resource-definitions.json
+python index.py ../recognizers-choice/resource-definitions.json
+python index.py ../recognizers-number/resource-definitions.json
+python index.py ../recognizers-number-with-unit/resource-definitions.json
+python index.py ../recognizers-date-time/resource-definitions.json
+python index.py ../recognizers-sequence/resource-definitions.json
 
 cd ../..
 echo // Installing recognizers-text
-$1/bin/python -m pip install -e ./libraries/recognizers-text/
+pip install -e ./libraries/recognizers-text/
 
 echo // Installing recognizers-number
-$1/bin/python -m pip install -e ./libraries/recognizers-number/
+pip install -e ./libraries/recognizers-number/
 
 echo // Installing recognizers-number-with-unit
-$1/bin/python -m pip install -e ./libraries/recognizers-number-with-unit/
+pip install -e ./libraries/recognizers-number-with-unit/
 
 echo // Installing datatypes-timex-expression
-$1/bin/python -m pip install -e ./libraries/datatypes-timex-expression/
+pip install -e ./libraries/datatypes-timex-expression/
 
 echo // Installing recognizers-date-time
-$1/bin/python -m pip install -e ./libraries/recognizers-date-time/
+pip install -e ./libraries/recognizers-date-time/
 
 echo // Installing recognizers-sequence
-$1/bin/python -m pip install -e ./libraries/recognizers-sequence/
+pip install -e ./libraries/recognizers-sequence/
 
 echo // Installing recognizers-choice
-$1/bin/python -m pip install -e ./libraries/recognizers-choice/
+pip install -e ./libraries/recognizers-choice/
 
 echo // Installing recognizers-suite
-$1/bin/python -m pip install -e ./libraries/recognizers-suite/
+pip install -e ./libraries/recognizers-suite/
 
 echo // Installing Test Dependencies
-$1/bin/pip install -r ./tests/requirements.txt
+pip install -r ./tests/requirements.txt
 
 echo // Running tests
-$1/bin/pytest --tb=line
+pytest --tb=line
