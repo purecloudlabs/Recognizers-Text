f5413c3b0f66d7df1496d052616e5f19d0b7a414
Kanchan Kumar <Kanchan.Kumar@genesys.com>
2023-01-09 12:30:46 +0000

    update package

--- Python/libraries/recognizers-date-time/setup.py
+++ Python/libraries/recognizers-date-time/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 NAME = 'recognizers-text-date-time-genesys'
 
-VERSION = '1.0.36'
+VERSION = '1.0.38a0'
 
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys',
             'recognizers-text-number-with-unit-genesys', 'regex', 'datedelta']
