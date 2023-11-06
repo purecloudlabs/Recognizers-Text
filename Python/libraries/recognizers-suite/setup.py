089f03c74b689486f1cedef4788305c40d3a5fb0
ckeaney <conor.keaney@genesys.com>
2023-11-06 17:14:27 +0000

    Fix for half past the hour

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.74'
+VERSION = '1.0.75a0'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.74',
-    'recognizers-text-number-genesys==1.0.74',
-    'recognizers-text-number-with-unit-genesys==1.0.74',
-    'recognizers-text-date-time-genesys==1.0.74',
-    'recognizers-text-sequence-genesys==1.0.74',
-    'recognizers-text-choice-genesys==1.0.74',
-    'datatypes_timex_expression_genesys==1.0.74'
+    'recognizers-text-genesys==1.0.75a0',
+    'recognizers-text-number-genesys==1.0.75a0',
+    'recognizers-text-number-with-unit-genesys==1.0.75a0',
+    'recognizers-text-date-time-genesys==1.0.75a0',
+    'recognizers-text-sequence-genesys==1.0.75a0',
+    'recognizers-text-choice-genesys==1.0.75a0',
+    'datatypes_timex_expression_genesys==1.0.75a0'
 ]
 
 setup(
