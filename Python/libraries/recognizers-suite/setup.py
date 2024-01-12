aa0dadf27dc9185e9ae0f529ec290a5e48610b89
andrew-gradinari <93652511+andrew-gradinari@users.noreply.github.com>
2024-01-12 16:13:16 +0000

    [NLU-4000] Arabic Number: Comma-Decimal bug (#114)

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.88'
+VERSION = '1.0.89'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.88',
-    'recognizers-text-number-genesys==1.0.88',
-    'recognizers-text-number-with-unit-genesys==1.0.88',
-    'recognizers-text-date-time-genesys==1.0.88',
-    'recognizers-text-sequence-genesys==1.0.88',
-    'recognizers-text-choice-genesys==1.0.88',
-    'datatypes_timex_expression_genesys==1.0.88'
+    'recognizers-text-genesys==1.0.89',
+    'recognizers-text-number-genesys==1.0.89',
+    'recognizers-text-number-with-unit-genesys==1.0.89',
+    'recognizers-text-date-time-genesys==1.0.89',
+    'recognizers-text-sequence-genesys==1.0.89',
+    'recognizers-text-choice-genesys==1.0.89',
+    'datatypes_timex_expression_genesys==1.0.89'
 ]
 
 setup(
