da35ce69573928e267475349610e83ac3524b6bb
KanchanKumar12 <118534451+KanchanKumar12@users.noreply.github.com>
2024-01-16 17:38:01 +0000

    [NLU-4008] [Part-I] Numeral words resolved incorrectly (#115)
    * changes
    
    * add tests
    
    * update tests
    
    * release versions
    
    * release versions

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.90'
+VERSION = '1.0.91'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.90',
-    'recognizers-text-number-genesys==1.0.90',
-    'recognizers-text-number-with-unit-genesys==1.0.90',
-    'recognizers-text-date-time-genesys==1.0.90',
-    'recognizers-text-sequence-genesys==1.0.90',
-    'recognizers-text-choice-genesys==1.0.90',
-    'datatypes_timex_expression_genesys==1.0.90'
+    'recognizers-text-genesys==1.0.91',
+    'recognizers-text-number-genesys==1.0.91',
+    'recognizers-text-number-with-unit-genesys==1.0.91',
+    'recognizers-text-date-time-genesys==1.0.91',
+    'recognizers-text-sequence-genesys==1.0.91',
+    'recognizers-text-choice-genesys==1.0.91',
+    'datatypes_timex_expression_genesys==1.0.91'
 ]
 
 setup(
