f3a525b02df1aef076279aa7658e52c93785c01c
Conor <93664980+Conor-Keaney@users.noreply.github.com>
2024-01-23 14:54:19 +0000

    [NLU-3965] - Catalan N of N dates  (#119)
    * Update timex
    
    * New prepositions, written days and months, testing
    
    * Updated versions
    
    * Removed debug line
    
    * Corrected regex formatting
    
    * Added deu to written one to twelve
    
    * Corrected written dates
    
    * Release version

--- Python/libraries/recognizers-date-time/recognizers_date_time/resources/catalan_date_time.py
+++ Python/libraries/recognizers-date-time/recognizers_date_time/resources/catalan_date_time.py
@@ -1,4 +1,6 @@
 from .base_date_time import BaseDateTime
+
+
 # pylint: disable=line-too-long
 
 
@@ -11,7 +13,7 @@ class CatalanDateTime:
     RangePrefixRegex = f'(des de|entre)'
     DayRegex = f'\\b(?<day>01|02|03|04|05|06|07|08|09|10|11|12|13|14|15|16|17|18|19|1|20|21|22|23|24|25|26|27|28|29|2|30|31|3|4|5|6|7|8|9)(?:\\.[º°])?(?=\\b|t)'
     MonthNumRegex = f'(?<month>1[0-2]|(0)?[1-9])\\b'
-    WrittenDayRegex = f'(?<day>un|dos|tres|quatre|cinc|cinc|sis|set|vuit|nou|nou|deu|onze|dotze|dotze|tretze|catorze|quinze|setze|mor disset|mor divuit|dinnou|vint|vint-i-un|vint-i-dos|vint-i-tres|vint-i-quatre|vint-i-cinc|vint-i-sis|vint-i-set|vint-i-vuit|vint-i-nou|trenta|trenta-un)'
+    WrittenDayRegex = f'(?<day>un?|dos|tres|quatre|cinc|sis|set|vuit|nou|deu|onze|dotze|tretze|catorze|quinze|setze|disset|divuit|dinou|vint|vint-i-un?|vint-i-dos|vint-i-tres|vint-i-quatre|vint-i-cinc|vint-i-sis|vint-i-set|vint-i-vuit|vint-i-nou|trenta|trenta-un?)'
     OclockRegex = f'(?<oclock>en\\s+punt(o)?)'
     AmDescRegex = f'({BaseDateTime.BaseAmDescRegex})'
     PmDescRegex = f'({BaseDateTime.BasePmDescRegex})'
@@ -20,6 +22,7 @@ class CatalanDateTime:
     OfPrepositionRegex = f'(\\bde\\b)'
     TwoDigitYearRegex = f'\\b(?<![$])(?<year>([0-9]\\d))(?!(\\s*((\\:\\d)|{AmDescRegex}|{PmDescRegex}|\\.\\d))|\\.?[º°ª])\\b'
     WrittenOneToNineRegex = f'(?:u(n)?|dos|tres|quatre|cinc|sis|set|vuit|nou)'
+    WrittenOneToTwelveRegex = f'(?<month>un?|dos|tres|quatre|cinc|sis|set|vuit|nou|deu|onze|dotze)'
     TwoToNineIntegerRegex = f'(?:tres|set|vuit|quatre|cinc|nou|dos|sis)'
     WrittenElevenToNineteenRegex = f'(?:onze|dotze|tretze|catorze|quinze|setze|disset|divuit|dinou)'
     WrittenTensRegex = f'(?:setanta|vint|trenta|vuitanta|noranta|quaranta|cinquanta|seixanta)'
@@ -30,17 +33,18 @@ class CatalanDateTime:
     YearRegex = f'({BaseDateTime.FourDigitYearRegex}|{FullTextYearRegex})'
     MonthRegex = f'\\b(?<month>gener|febrer|mar[çc]|abril|maig|juny|juliol|agost|setembre|octubre|novembre|desembre)'
     MonthSuffixRegex = f'(?<msuf>((a|de)\\s+)?({MonthRegex}))'
-    SimpleCasesRegex = f'\\b({RangePrefixRegex}\\s+)?({DayRegex})\\s*{TillRegex}\\s*({DayRegex})\\s+{MonthSuffixRegex}((\\s+|\\s*,\\s*){YearRegex})?\\b'
+    YearSuffixRegex = f'((\\s?(a|del?|(de\\sl\'|del\\s)any)\\s+)?({YearRegex}|{FullTextYearRegex}))'
+    SimpleCasesRegex = f'\\b({RangePrefixRegex}\\s+)?({DayRegex}|{WrittenDayRegex})\\s*{TillRegex}\\s*({DayRegex}|{WrittenDayRegex})\\s+{MonthSuffixRegex}((\\s+|\\s*,\\s*){YearRegex})?\\b'
     MonthFrontSimpleCasesRegex = f'\\b{MonthSuffixRegex}\\s+({RangePrefixRegex}?({DayRegex})\\s*{TillRegex}\\s*({DayRegex})((\\s+|\\s*,\\s*){YearRegex})?\\b'
     MonthFrontBetweenRegex = f'\\b{MonthSuffixRegex}\\s+({RangePrefixRegex}({DayRegex})\\s*{RangeConnectorRegex}\\s*({DayRegex})((\\s+|\\s*,\\s*){YearRegex})?\\b'
     MonthNumWithYearRegex = f'\\b(({YearRegex}(\\s*?)[/\\-\\.~](\\s*?){MonthNumRegex})|({MonthNumRegex}(\\s*?)[/\\-\\.~](\\s*?){YearRegex}))\\b'
-    OfYearRegex = f'\\b((de?)\\s+({YearRegex}\\s+any))\\b'
+    OfYearRegex = f'\\b(((de?)\\s+(({YearRegex}\\s+any)))|(de(\\s+l\'|l\\s)any\\s+{YearRegex}))\\b'
     CenturySuffixRegex = f'(^segle)\\b'
     RangeUnitRegex = f'\\b(?<unit>anys?|mesos?|setmanes?)\\b'
     BeforeAfterRegex = f'^[.]'
     InConnectorRegex = f'\\b(en)(?=\\s*$)\\b'
     TodayNowRegex = f'\\b(avui|ara)\\b'
-    FromRegex = f'((\\bdes?)(\\s*del)?)$'
+    FromRegex = f'((\\bde(s|l)?)(\\s*al?)?)$'
     BetweenRegex = f'(\\bentre\\s*)'
     WeekDayRegex = f'\\b(?<weekday>(dilluns|dimarts|dimecres|dijous|divendres|dissabte|diumenge))\\b'
     OnRegex = f'(?<=\\bel(\\s+d[ií]a)?\\s+)({DayRegex}s?)(?![.,]\\d)\\b'
@@ -68,7 +72,7 @@ class CatalanDateTime:
     WeekDayStart = f'^\\b$'
     DateYearRegex = f'(?<year>{YearRegex}|(?<!,\\s?){TwoDigitYearRegex}|{TwoDigitYearRegex}(?=(\\.(?!\\d)|[?!;]|$)))'
     DateExtractor1 = f'\\b({WeekDayRegex}(\\s+|\\s*,\\s*))?(?<!\\d[.,]){DayRegex}((\\s*(d[e\'])|[/\\\\\\.\\-])\\s*)?{MonthRegex}\\b'
-    DateExtractor2 = f'\\b((el\\s+d[ií]a|{WeekDayRegex})(\\s+|\\s*,\\s*))?(?<!\\d[.,])(({DayRegex}((\\s+(d[e\']\\s?)?|\\s*[.,/-]\\s*){MonthRegex}((\\s+(del?\\s+)?|\\s*[.,/-]\\s*){DateYearRegex}\\b)?|\\s+(d[e\']\\s?){MonthNumRegex}\\s+(del?\\s+{DateYearRegex}\\b)))|{BaseDateTime.FourDigitYearRegex}\\s*[.,/-]?\\s*(el\\s+d[ií]a\\s+)?{DayRegex}(\\s+(d[e\']\\s+)?|\\s*[.,/-]\\s*){MonthRegex})'
+    DateExtractor2 = f'\\b((el\\s+d[ií]a|{WeekDayRegex})(\\s+|\\s*,\\s*))?(?<!\\d[.,])((({DayRegex}|{WrittenDayRegex})((\\s+(d(\'|e(l|\\sl\')?)\\s?)?|\\s*[.,/-]\\s*)({MonthRegex}|{MonthNumRegex}|{WrittenOneToTwelveRegex})((\\s+(de(l|l\\sany|\\sl\'any)?\\s+)?|\\s*[.,/-]\\s*){DateYearRegex}\\b)?|\\s+(d[e\']\\s?){MonthNumRegex}\\s+(del?\\s+{DateYearRegex}\\b)))|{BaseDateTime.FourDigitYearRegex}\\s*[.,/-]?\\s*(el\\s+d[ií]a\\s+)?{DayRegex}(\\s+(d[e\']\\s+)?|\\s*[.,/-]\\s*){MonthRegex})'
     DateExtractor3 = f'\\b({WeekDayRegex}(\\s+|\\s*,\\s*))?{MonthRegex}(\\s*[.,/-]?\\s*)(el\\s+d[ií]a\\s+)?{DayRegex}(?!\\s*\\-\\s*\\d{{2}}\\b)((\\s+(del?\\s+)?|\\s*[.,/-]\\s*){DateYearRegex})?\\b'
     DateExtractor4 = f'\\b(?<!\\d[.,]){MonthNumRegex}\\s*[/\\\\\\-]\\s*{DayRegex}\\s*[/\\\\\\-]\\s*{DateYearRegex}(?!\\s*[/\\\\\\-\\.]\\s*\\d+)'
     DateExtractor5 = f'\\b(?<!\\d[.,]){DayRegex}\\s*[/\\\\\\-\\.]\\s*({MonthNumRegex}|{MonthRegex})\\s*[/\\\\\\-\\.]\\s*{DateYearRegex}(?!\\s*[/\\\\\\.]\\s*\\d+)'
@@ -155,8 +159,27 @@ class CatalanDateTime:
                         ("06", 6),
                         ("07", 7),
                         ("08", 8),
-                        ("09", 9)])
+                        ("09", 9),
+                        ("u", 1),
+                        ("un", 1),
+                        ("una", 1),
+                        ("dos", 2),
+                        ("tres", 3),
+                        ("trés", 3),
+                        ("quatre", 4),
+                        ("cinc", 5),
+                        ("sis", 6),
+                        ("set", 7),
+                        ("vuit", 8),
+                        ("nou", 9),
+                        ("deu", 10),
+                        ("onze", 11),
+                        ("dotze", 12),
+                        ("docena", 12),
+                        ("dotzenes", 12),
+                        ])
     Numbers = dict([("zero", 0),
+                    ("u", 1),
                     ("un", 1),
                     ("una", 1),
                     ("dos", 2),
@@ -199,5 +222,37 @@ class CatalanDateTime:
                     ("treinta", 30),
                     ("quaranta", 40),
                     ("cinquanta", 50)])
+    DayOfMonth = dict([("u", 1),
+                       ("un", 1),
+                       ("dos", 2),
+                       ("tres", 3),
+                       ("quatre", 4),
+                       ("cinc", 5),
+                       ("sis", 6),
+                       ("set", 7),
+                       ("vuit", 8),
+                       ("nou", 9),
+                       ("deu", 10),
+                       ("onze", 11),
+                       ("dotze", 12),
+                       ("tretze", 13),
+                       ("catorze", 14),
+                       ("quinze", 15),
+                       ("setze", 16),
+                       ("disset", 17),
+                       ("divuit", 18),
+                       ("dinou", 19),
+                       ("vint", 20),
+                       ("vint-i-un", 21),
+                       ("vint-i-dos", 22),
+                       ("vint-i-tres", 23),
+                       ("vint-i-quatre", 24),
+                       ("vint-i-cinc", 25),
+                       ("vint-i-sis", 26),
+                       ("vint-i-set", 27),
+                       ("vint-i-vuit", 28),
+                       ("vint-i-nou", 29),
+                       ("trenta", 30),
+                       ("trenta-un", 31)])
     DefaultLanguageFallback = 'DMY'
 # pylint: enable=line-too-long
