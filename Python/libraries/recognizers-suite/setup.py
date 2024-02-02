3bf55c37af2e809a8ab7acfbb12566a237a36fbc
Conor <93664980+Conor-Keaney@users.noreply.github.com>
2024-02-02 13:45:42 +0000

    NLU-4051 - Fixing Failing Arabic amountOfMoney Regression tests (#122)
    * Update timex
    
    * Fix for 7 tests in arabic amount of money
    
    * 3 other fixes and added test cases
    
    * Removed regex that was for testing purposes
    
    * Updated versions
    
    * Added 0 to 9 in arabic script
    
    * Removed arabic numerals as they are covered under \d
    
    * Update package version
    
    * Added Pounds to regex and test
    
    * Corrected piasters to be a subtype of pennies rather than cents
    
    * Update to release version

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.0.100'
+VERSION = '1.1.1'
 REQUIRES = [
-    'recognizers-text-genesys==1.0.100',
-    'recognizers-text-number-genesys==1.0.100',
-    'recognizers-text-number-with-unit-genesys==1.0.100',
-    'recognizers-text-date-time-genesys==1.0.100',
-    'recognizers-text-sequence-genesys==1.0.100',
-    'recognizers-text-choice-genesys==1.0.100',
-    'datatypes_timex_expression_genesys==1.0.100'
+    'recognizers-text-genesys==1.1.1',
+    'recognizers-text-number-genesys==1.1.1',
+    'recognizers-text-number-with-unit-genesys==1.1.1',
+    'recognizers-text-date-time-genesys==1.1.1',
+    'recognizers-text-sequence-genesys==1.1.1',
+    'recognizers-text-choice-genesys==1.1.1',
+    'datatypes_timex_expression_genesys==1.1.1'
 ]
 
 setup(
