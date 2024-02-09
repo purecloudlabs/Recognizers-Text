3bce1d2940f99f97f5ef32f63a4420b5bb86fac1
Kanchan Kumar <Kanchan.Kumar@genesys.com>
2024-02-09 11:51:06 +0000

    release package versions

--- Python/libraries/recognizers-sequence/setup.py
+++ Python/libraries/recognizers-sequence/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-sequence-genesys"
-VERSION = "1.1.4a0"
+VERSION = "1.1.4"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.4a0'
+VERSION = '1.1.4'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.4a0',
-    'recognizers-text-number-genesys==1.1.4a0',
-    'recognizers-text-number-with-unit-genesys==1.1.4a0',
-    'recognizers-text-date-time-genesys==1.1.4a0',
-    'recognizers-text-sequence-genesys==1.1.4a0',
-    'recognizers-text-choice-genesys==1.1.4a0',
-    'datatypes_timex_expression_genesys==1.1.4a0'
+    'recognizers-text-genesys==1.1.4',
+    'recognizers-text-number-genesys==1.1.4',
+    'recognizers-text-number-with-unit-genesys==1.1.4',
+    'recognizers-text-date-time-genesys==1.1.4',
+    'recognizers-text-sequence-genesys==1.1.4',
+    'recognizers-text-choice-genesys==1.1.4',
+    'datatypes_timex_expression_genesys==1.1.4'
 ]
 
 setup(
