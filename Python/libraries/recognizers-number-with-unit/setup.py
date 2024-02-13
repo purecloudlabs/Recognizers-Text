f6e260bd8aed0547c700338b7263ac4fd813d8ae
Bobby Martins <bobby.martins@genesys.com>
2024-02-13 10:10:09 +0000

    ++ updated package versions

--- Python/libraries/recognizers-number-with-unit/setup.py
+++ Python/libraries/recognizers-number-with-unit/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-with-unit-genesys"
-VERSION = "1.1.5a0"
+VERSION = "1.1.7a0"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
