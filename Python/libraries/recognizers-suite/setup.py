e40a221816a0138c7465ba18ab2261673c0d4e92
Sam Hickey <sam.hickey@genesys.com>
2024-02-15 16:53:39 +0000

    Fix for arabic this <weekday>

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.6'
+VERSION = '1.1.9a0'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.6',
-    'recognizers-text-number-genesys==1.1.6',
-    'recognizers-text-number-with-unit-genesys==1.1.6',
-    'recognizers-text-date-time-genesys==1.1.6',
-    'recognizers-text-sequence-genesys==1.1.6',
-    'recognizers-text-choice-genesys==1.1.6',
-    'datatypes_timex_expression_genesys==1.1.6'
+    'recognizers-text-genesys==1.1.9a0',
+    'recognizers-text-number-genesys==1.1.9a0',
+    'recognizers-text-number-with-unit-genesys==1.1.9a0',
+    'recognizers-text-date-time-genesys==1.1.9a0',
+    'recognizers-text-sequence-genesys==1.1.9a0',
+    'recognizers-text-choice-genesys==1.1.9a0',
+    'datatypes_timex_expression_genesys==1.1.9a0'
 ]
 
 setup(
