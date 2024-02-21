7c8f517a3f319acd10a270a08d5d7cc7ae11ad31
Sam Hickey <sam.hickey@genesys.com>
2024-02-21 17:37:28 +0000

    Increment package version

--- Python/libraries/recognizers-date-time/setup.py
+++ Python/libraries/recognizers-date-time/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-date-time-genesys'
-VERSION = '1.1.9'
+VERSION = '1.1.13a0'
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys',
             'recognizers-text-number-with-unit-genesys', 'regex', 'datedelta', 'python-dateutil']
 
