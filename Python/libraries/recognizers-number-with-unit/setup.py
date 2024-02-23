fc8ccb09b9121ea8f8867a9e49d119988ba6984a
Kanchan Kumar <Kanchan.Kumar@genesys.com>
2024-02-23 09:55:51 +0000

    release versions

--- Python/libraries/recognizers-number-with-unit/setup.py
+++ Python/libraries/recognizers-number-with-unit/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-with-unit-genesys"
-VERSION = "1.1.10a1"
+VERSION = "1.1.10"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
