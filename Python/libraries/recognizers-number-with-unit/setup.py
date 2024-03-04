86a32d4ed8072641b5819554976165a7e2504230
Bobby Martins <bobby.martins@genesys.com>
2024-03-04 18:00:48 +0000

    ++ removed backslashes from parenthesised ```.``` and also package versions

--- Python/libraries/recognizers-number-with-unit/setup.py
+++ Python/libraries/recognizers-number-with-unit/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-with-unit-genesys"
-VERSION = "1.1.22"
+VERSION = "1.1.23a0"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
