d4207b622b4ccb96bd177b4b82ac9a158f23def0
Andrew Gradinari <andrew.gradinari@genesys.com>
2024-03-04 11:14:31 +0000

    Update package versions

--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-choice-genesys'
-VERSION = '1.1.21'
+VERSION = '1.1.22'
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
 setup(
--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.21'
+VERSION = '1.1.22'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.21',
-    'recognizers-text-number-genesys==1.1.21',
-    'recognizers-text-number-with-unit-genesys==1.1.21',
-    'recognizers-text-date-time-genesys==1.1.21',
-    'recognizers-text-sequence-genesys==1.1.21',
-    'recognizers-text-choice-genesys==1.1.21',
-    'datatypes_timex_expression_genesys==1.1.21'
+    'recognizers-text-genesys==1.1.22',
+    'recognizers-text-number-genesys==1.1.22',
+    'recognizers-text-number-with-unit-genesys==1.1.22',
+    'recognizers-text-date-time-genesys==1.1.22',
+    'recognizers-text-sequence-genesys==1.1.22',
+    'recognizers-text-choice-genesys==1.1.22',
+    'datatypes_timex_expression_genesys==1.1.22'
 ]
 
 setup(
