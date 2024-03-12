8468c0d79778c9427c3bd7a9a15fefe0fe905580
CiaraCrowe <Ciara.crowe@genesys.com>
2024-03-12 08:23:13 +0000

    Updated versions

--- Python/libraries/recognizers-choice/setup.py
+++ Python/libraries/recognizers-choice/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-choice-genesys'
-VERSION = '1.1.25a0'
+VERSION = '1.1.26a0'
 REQUIRES = ['recognizers-text-genesys', 'regex', 'grapheme']
 
 setup(
--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.1.25a0"
+VERSION = "1.1.26a0"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
