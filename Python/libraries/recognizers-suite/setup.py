2dd8a4e13ee1803ecc771319ce5d0d805677e7e1
CiaraCrowe <Ciara.crowe@genesys.com>
2024-03-12 12:34:42 +0000

    Updated versions, got rid of alpha

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.26a1'
+VERSION = '1.1.26'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.26a1',
-    'recognizers-text-number-genesys==1.1.26a1',
-    'recognizers-text-number-with-unit-genesys==1.1.26a1',
-    'recognizers-text-date-time-genesys==1.1.26a1',
-    'recognizers-text-sequence-genesys==1.1.26a1',
-    'recognizers-text-choice-genesys==1.1.26a1',
-    'datatypes_timex_expression_genesys==1.1.26a1'
+    'recognizers-text-genesys==1.1.26',
+    'recognizers-text-number-genesys==1.1.26',
+    'recognizers-text-number-with-unit-genesys==1.1.26',
+    'recognizers-text-date-time-genesys==1.1.26',
+    'recognizers-text-sequence-genesys==1.1.26',
+    'recognizers-text-choice-genesys==1.1.26',
+    'datatypes_timex_expression_genesys==1.1.26'
 ]
 
 setup(
