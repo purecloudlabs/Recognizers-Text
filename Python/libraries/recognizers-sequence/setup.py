0cbcf8f3f0c0bf6b1b7457c77d46208d45f9f476
Sam Hickey <sam.hickey@genesys.com>
2024-04-03 16:47:40 +0100

    Update package version

--- Python/libraries/recognizers-sequence/setup.py
+++ Python/libraries/recognizers-sequence/setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-sequence-genesys"
-VERSION = "1.1.29a0"
+VERSION = "1.1.29"
 REQUIRES = ['recognizers-text-genesys', 'recognizers-text-number-genesys', 'regex']
 
 setup(
