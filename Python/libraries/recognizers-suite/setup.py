8d69a9123d484fd255a6166de305d8e477bca63b
Conor <93664980+Conor-Keaney@users.noreply.github.com>
2024-04-18 14:03:34 +0100

    NLU-4235 - ES-ES Date - fix for 'barra' word date seperator not resolving written dates (#153)
    * Update timex
    
    * Fix for written dates with barr
    
    * Update package versions
    
    * Update version to release

--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -10,15 +10,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.32'
+VERSION = '1.1.33'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.32',
-    'recognizers-text-number-genesys==1.1.32',
-    'recognizers-text-number-with-unit-genesys==1.1.32',
-    'recognizers-text-date-time-genesys==1.1.32',
-    'recognizers-text-sequence-genesys==1.1.32',
-    'recognizers-text-choice-genesys==1.1.32',
-    'datatypes_timex_expression_genesys==1.1.32'
+    'recognizers-text-genesys==1.1.33',
+    'recognizers-text-number-genesys==1.1.33',
+    'recognizers-text-number-with-unit-genesys==1.1.33',
+    'recognizers-text-date-time-genesys==1.1.33',
+    'recognizers-text-sequence-genesys==1.1.33',
+    'recognizers-text-choice-genesys==1.1.33',
+    'datatypes_timex_expression_genesys==1.1.33'
 ]
 
 setup(
