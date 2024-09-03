47ed5d1da154c9a90855d0b413404a3eb594b9ea
ckeaney <conor.keaney@genesys.com>
2024-09-03 15:12:40 +0100

    Update version to release

--- Python/libraries/recognizers-number/setup.py
+++ Python/libraries/recognizers-number/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = "recognizers-text-number-genesys"
-VERSION = "1.1.36a1"
+VERSION = "1.1.36"
 REQUIRES = ['recognizers-text-genesys', 'regex']
 
 setup(
--- Python/libraries/recognizers-suite/setup.py
+++ Python/libraries/recognizers-suite/setup.py
@@ -11,15 +11,15 @@ def read(fname):
 
 
 NAME = 'recognizers-text-suite-genesys'
-VERSION = '1.1.36a1'
+VERSION = '1.1.36'
 REQUIRES = [
-    'recognizers-text-genesys==1.1.36a1',
-    'recognizers-text-number-genesys==1.1.36a1',
-    'recognizers-text-number-with-unit-genesys==1.1.36a1',
-    'recognizers-text-date-time-genesys==1.1.36a1',
-    'recognizers-text-sequence-genesys==1.1.36a1',
-    'recognizers-text-choice-genesys==1.1.36a1',
-    'datatypes_timex_expression_genesys==1.1.36a1',
+    'recognizers-text-genesys==1.1.36',
+    'recognizers-text-number-genesys==1.1.36',
+    'recognizers-text-number-with-unit-genesys==1.1.36',
+    'recognizers-text-date-time-genesys==1.1.36',
+    'recognizers-text-sequence-genesys==1.1.36',
+    'recognizers-text-choice-genesys==1.1.36',
+    'datatypes_timex_expression_genesys==1.1.36',
 ]
 
 setup(
