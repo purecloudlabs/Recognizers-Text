9aa48021fe27b293879d32791289f8d6787b5769
Ciara Crowe <ciara.crowe@genesys.com>
2026-03-03 15:42:27 +0000

    reconginzers

--- Python/libraries/recognizers-date-time/setup.py
+++ Python/libraries/recognizers-date-time/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 NAME = 'recognizers-text-date-time-genesys'
-VERSION = '1.1.43'
+VERSION = '1.1.44'
 REQUIRES = [
     'recognizers-text-genesys',
     'recognizers-text-number-genesys',
